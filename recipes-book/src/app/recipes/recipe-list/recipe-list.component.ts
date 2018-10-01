040cfdade75b2cc5042421b29ed3069b64c12c21
Ariel-Git <arielgarfield.work@gmail.com>
2018-10-02 00:13:30 +0300

    recipes book project

--- recipes-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+	recipes: Recipe[] = [
+	new Recipe('A Test Rescipe','This is siply a test','https://images.theconversation.com/files/180401/original/file-20170731-22175-67v3q2.jpg?ixlib=rb-1.1.0&rect=0%2C589%2C5472%2C2654&q=45&auto=format&w=1356&h=668&fit=crop'),
+	new Recipe('A Test Rescipe2','This is siply a test2','https://images.theconversation.com/files/180401/original/file-20170731-22175-67v3q2.jpg?ixlib=rb-1.1.0&rect=0%2C589%2C5472%2C2654&q=45&auto=format&w=1356&h=668&fit=crop')
+	
+	];
 
   constructor() { }
 
