c415f03443de0679d2c0649dd0cebcc7d15804b9
ariel-git <arielmgarfield@gmail.com>
2018-12-10 17:49:15 +0200

    update 10.12.18

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { AlertModule } from 'ngx-bootstrap';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    //CommonModule,
     FormsModule,
     AlertModule.forRoot()
   ],
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
-
+ 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-	hero: Hero = {
-  		id:1,
-  		name:'windstorm'
-  		}
-onSelect(hero:Hero): void {
-  this.selectedHero = hero;
-}
-  constructor() { 
-  	//console.log(this.hero);
-  }
-
+ 
+  selectedHero: Hero;
+ 
+  heroes: Hero[];
+ 
+  constructor(private heroService: HeroService) { }
+ 
   ngOnInit() {
+    this.getHeroes();
   }
-
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+ 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
