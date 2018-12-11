7370546d45e630837ac19cacbe1c3d6c7f8b797d
ariel-git <arielmgarfield@gmail.com>
2018-12-11 18:00:31 +0200

    update tour of heroes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { AlertModule } from 'ngx-bootstrap';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessagesService} from './messages.service';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-	getHeroes(): Hero[]{
+	/*getHeroes(): Hero[]{
 		return HEROES;
+	}*/
+	getHeroes():Observable<Hero[]> {
+		this.messagesService.add('HeroService: fetched heroes');
+		return of(HEROES);
 	}
-  constructor() { }
+  constructor(private messagesService: MessagesService){ }
+	 	 getHero(id: number): Observable<Hero> {
+	 	 // TODO: send the message _after_ fetching the hero
+	 	 this.messagesService.add(`HeroService: fetched hero id=${id}`);
+		  return of(HEROES.find(hero => hero.id === id));
+	}
 }
