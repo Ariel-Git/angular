ba5c5f781dfb236ac3ee105e55b3e30836f69396
ariel-git <arielmgarfield@gmail.com>
2018-12-17 17:42:35 +0200

    update angular

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Hero} from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
 import { HeroService }  from '../hero.service';
 
 @Component({
@@ -27,6 +26,9 @@ getHero(): void {
   const id = +this.route.snapshot.paramMap.get('id');
   this.heroService.getHero(id)
     .subscribe(hero => this.hero = hero);
+}
+goBack():void{
+	this.location.back();
 }
 
 }
